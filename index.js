10cfaab714b5cb497252212c2e66f292b5a754f8
DS85300 <sagan@parkstern.de>
2025-04-30 17:21:35 +0200

    is running now

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
 
+dotenv.config();
+const sequelize = new Sequelize(process.env.PG_URI, {
+    dialect: 'postgres',
+  });
+
+//test if database is running
+sequelize.authenticate()
+  .then(() => console.log('Database connected successfully'))
+  .catch(err => console.error('Unable to connect to the database:', err));
+  
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5678
 
+
+
+
 app.use('/',(req,res) => {
 
-    app.res('Hello from the E-Commerce-Server')
+    res.send('Hello from the E-Commerce-Server')
 })
 
 
